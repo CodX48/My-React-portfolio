20faaf5992c930d3d0c2ee4f3167525345082b82
Codx48 <codx207@gmail.com>
2025-01-30 17:04:28 +0300

    i change the style of the project list i add modern bg

--- src/myprojects.jsx
+++ src/myprojects.jsx
@@ -36,12 +36,17 @@ function ReposList() {
 
     return(
         <div className="flex flex-col justify-center items-center mt-20 lg:w-full ">
-        
-        <ul className="flex flex-col justify-between items-center gap-5 w-[90%] bg-zinc-950/50 lg:px-20 py-20 ring-1 ring-pink-500/20 shadow-lg rounded-md md:px-10 sm:px-10 max-sm:px-10 min-h-[400px]">
+        <ul className="flex relative flex-col justify-between items-center gap-5 w-[90%] bg-zinc-950/50 lg:px-20 py-20 ring-1 ring-pink-500/20 shadow-lg rounded-md md:px-10 sm:px-10 max-sm:px-10 min-h-[400px] overflow-hidden">
+        <div className="absolute -left-[30%] -top-5 -rotate-12  w-[900px] h-[300px] bg-sky-700 rounded-full blur-3xl"></div>
+        <div className="z-20 absolute  animate-[--my-bounce] duration-1000 bg-sky-700/60 -rotate-45 right-[15%] w-[130px] h-[150px] top-[10%] rounded-full blur-2xl" style={{"--my-bounce": "bounce 3s infinite alternate"}}></div>
+        <div className=" absolute animate-[--my-bounce] duration-1000 bg-pink-900/50 -rotate-45 right-[15%] w-[100px] h-[100px] top-[10%] rounded-full blur-3xl" style={{"--my-bounce": "bounce 4s infinite alternate"}}></div>
+        <div className="z-30 absolute animate-[--my-bounce] duration-1000 bg-pink-900 -rotate-90 right-[12%] w-[130px] h-[150px] top-[10%] rounded-full blur-2xl" style={{"--my-bounce": "bounce 2.5s infinite alternate"}}></div>
+        <div className="absolute -right-[20%] -bottom-10 rotate-12 w-[900px] h-[350px] bg-pink-950 rounded-full blur-3xl"></div>
+                
           {FilterdRepos.map((Repos) => (
             <li
               key={Repos.id}
-              className="w-[100%] p-2"
+              className="w-[100%] p-2 z-50"
               >
               <div className="w-[100%] h-full lg:flex lg:flex-row lg:justify-between lg:items-center gap-5 md:flex-col md:items-start md:justify-center sm:block max-sm:block">
                 <a
