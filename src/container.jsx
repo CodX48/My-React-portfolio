63b9221dc338dd73cfb17425a784eba175f15789
Codx48 <codx207@gmail.com>
2025-02-01 21:04:29 +0300

    i add a contact section

--- src/container.jsx
+++ src/container.jsx
@@ -2,6 +2,7 @@ import { _Header } from "./header"
 import { Information_experians } from "./Myinfo"
 import { ReposList } from "./myprojects"
 import { MoreAboutMe } from "./About"
+import { ContactInfo } from "./contact"
 function _Container(){
     return(
         <div className=" text-zinc-100 "> 
@@ -13,6 +14,7 @@ function _Container(){
         <Information_experians />
         <ReposList />
         <MoreAboutMe />
+        <ContactInfo />
         </div>
         </div>
         <div class="absolute bottom-[-5%] right-[-10%] top-[-5%] h-[400px] w-[400px] md:h-[500px] md:w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
